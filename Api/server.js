47d634afdf0d19e2f0bb1afe158060bee2e6445f
Darkpayne <joshuaclifford4336@yahoo.com>
2022-07-29 14:23:06 +0100

    done auth. im working on backend api

--- Api/server.js
+++ Api/server.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
+port = 6060;
+
+dotenv.config();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+})
+.then(console.log("connected to MongoDB"))
+.catch((err)=>console.log(err));
 
 app.get("/", (req, res) => {
   res.send("everything is okay!");
 });
 
-port = 6060;
+app.use("/api/auth", authRoute);
+
 app.listen(port, () => {
   console.log(`server is listening on port ${port}`);
 });
