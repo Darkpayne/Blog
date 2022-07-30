fd6da79a7c2411a2629175a8a792f6f5a1bb0cdc
joshua clifford <joshuaclifford4336@yahoo.com>
2022-07-30 19:20:14 +0100

    just commiting

--- Api/routes/users.js
+++ Api/routes/users.js
@@ -1,19 +1,23 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 // UPDATE
 router.put("/:id", async (req, res) => {
   if (req.body.userId === req.params.id) {
-    if (req.body.password){
+    if (req.body.password) {
       const salt = await bcrypt.genSalt(10);
       req.body.password = await bcrypt.hash(req.body.password, salt);
     }
     try {
-      const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-        $set:req.body,
-      }, {new:true});
-      res.status(200).json(updatedUser)
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
     } catch (error) {
       res.status(500).json(error);
     }
@@ -26,9 +30,9 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   if (req.body.userId === req.params.id) {
     try {
-     await User.findByIdAndDelete(req.params.id);
-     res.status(200).json({message:"User deleted Succesfully.."})
-    //  you can still see this users posts 43:00 
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json({ message: "User deleted Succesfully.." });
+      //  you can still see this users posts 43:00
     } catch (error) {
       res.status(500).json(error);
     }
@@ -38,13 +42,24 @@ router.delete("/:id", async (req, res) => {
 });
 
 // GET USER
-router.get('/:id', async (req,res)=>{
+router.get("/:id", async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
-    const {password, ...others} = user._doc
+    const { password, ...others } = user._doc;
     res.status(200).json(others);
   } catch (error) {
-    res.status(500).json({message:"error"})
+    res.status(500).json({ message: "error" });
   }
-})
+});
+
+// GET ALL USERS
+
+router.get("/", async (req, res) => {
+  try {
+    const allUsers = await User.find();
+    res.status(200).json(allUsers);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 module.exports = router;
--- Api/server.js
+++ Api/server.js
@@ -1,38 +1,39 @@
 const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const authRoute = require('./routes/auth');
-const userRoute = require('./routes/users');
-const postRoute = require('./routes/posts');
-const categoryRoute = require('./routes/categories');
-const multer = require('multer');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 port = 6060;
 
 dotenv.config();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser:true,
-  useUnifiedTopology:true,
-})
-.then(console.log("connected to MongoDB"))
-.catch((err)=>console.log(err));
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 // uploading a file/picture
 
 const storage = multer.diskStorage({
-  destination:(req,file,cb) =>{
-    cb(null, "images")
-  }, filename:(req,file,cb)=>{
-    //cb(null, req.body.name); 
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    //cb(null, req.body.name);
     cb(null, "hello.jpeg");
-    
-  }
-})
+  },
+});
 
-const upload = multer({storage:storage});
-app.post("/api/upload", upload.single("file"), (req,res)=>{
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
   res.status(200).json("file has been uploaded");
 });
 
