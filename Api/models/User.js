6be23b2656c03abe5568987d2f42ea8ac5995e1f
joshua clifford <joshuaclifford4336@yahoo.com>
2022-08-24 05:16:35 +0100

    finally started working on the roles and token

--- Api/models/User.js
+++ Api/models/User.js
@@ -1,20 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type:String,
-        required:true
-    }
-}, {timestamps: true})
+    password: {
+      type: String,
+      required: true,
+    },
+    roles: {
+      User: {
+        type: Number,
+        default: 2001,
+      },
+      Editor: {
+        type: Number,
+      },
+      Admin: {
+        type: Number,
+      },
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports=mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", UserSchema);
